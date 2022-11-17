ed4ccfe6e99af669b6e2eb02f03444287f656e1b
Code Sandbox <hello@codesandbox.io>
2022-11-17 13:30:12 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.1cbfb85d42eb51e8d7d2418e4ac6d920.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4113ff299f73858faff1",
+    "url": "https://p0cket.github.io/csb-3f8fzh/static/css/main.5e3f3553.chunk.css"
+  },
+  {
+    "revision": "4113ff299f73858faff1",
+    "url": "https://p0cket.github.io/csb-3f8fzh/static/js/main.a0869f61.chunk.js"
+  },
+  {
+    "revision": "be147f098713347fc1dc",
+    "url": "https://p0cket.github.io/csb-3f8fzh/static/js/runtime~main.ddb4ba6f.js"
+  },
+  {
+    "revision": "bfbfbedaf888393ce43f",
+    "url": "https://p0cket.github.io/csb-3f8fzh/static/js/2.55d8913a.chunk.js"
+  },
+  {
+    "revision": "519b2af0c5c8f226bebd468bfc194eac",
+    "url": "https://p0cket.github.io/csb-3f8fzh/index.html"
+  }
+];
